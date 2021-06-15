2d94cb3ea8357e79f71099ec26d7c4d22abb716d
Thiago Elias <thiagoe.ssp@gmail.com>
2021-06-14 23:23:42 -0300

    init logic

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+const boxBoard = document.querySelector('.board');
+
+const home1 = document.getElementById('1')
+const home2 = document.getElementById('2')
+const home3 = document.getElementById('3')
+const home4 = document.getElementById('4')
+const home5 = document.getElementById('5')
+const home6 = document.getElementById('6')
+const home7 = document.getElementById('7')
+const home8 = document.getElementById('8')
+const home9 = document.getElementById('9')
+
+boxBoard.addEventListener( 'click', console.log());
