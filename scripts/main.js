1a6b9a6e626b6949fe140e08f9354e9111be90b6
Thiago Elias <thiagoe.ssp@gmail.com>
2021-06-15 21:22:21 -0300

    UI tic-tac-toe 90%

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,23 @@
 const boxBoard = document.querySelector('.board');
 
-const home1 = document.getElementById('1')
-const home2 = document.getElementById('2')
-const home3 = document.getElementById('3')
-const home4 = document.getElementById('4')
-const home5 = document.getElementById('5')
-const home6 = document.getElementById('6')
-const home7 = document.getElementById('7')
-const home8 = document.getElementById('8')
-const home9 = document.getElementById('9')
 
-boxBoard.addEventListener( 'click', console.log());
+
+
+
+
+
+/*
+function move(id){
+    const home = id.target
+    const icon = document.createElement('img')
+    if(home.childElementCount == 0){
+        icon.src = "../icons/close_24px_outlined.svg"
+    }
+
+    home.appendChild(icon)
+
+    console.log(home)
+}
+
+boxBoard.addEventListener( 'click', move);
+*/
